b6e8abce99e1f9c66ea60a54e5341a2203becb7c
jennojenno <jennolim@gmail.com>
2016-02-04 16:11:12 -0500

    Basics of attack working

--- rpg-oop/ViewController.swift
+++ rpg-oop/ViewController.swift
@@ -16,15 +16,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var enemyImg: UIImageView!
     @IBOutlet weak var chestBtn: UIButton!
     
-    @IBAction func onChestClick(sender: AnyObject) {
-        
-    }
+    //We will def have a player and enemy so !
+    var player: Player!
+    var enemy: Enemy!
+    //Even if I wanted a chimera, I can start with type Enemy and initialize a chimera
     
+    var chestMessage: String?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "Gizmo", hp: 110, attackPwr: 23)
+        playerHpLabel.text = "\(player.hp) HP"
+        
+        generateRandomEnemy()
+    }
+    
+    //Want to generate a random enemy: 
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        if rand == 0 {
+            enemy = Chimera(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+        
+        enemyImg.hidden = false 
+    }
+    
+    @IBAction func onChestClick(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLabel.text = chestMessage
+        
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "generateRandomEnemy", userInfo: nil, repeats: false)
+        
+    }
+    @IBAction func attackClicked(sender: AnyObject) {
+        if enemy.attemptAttack(player.attackPwr) {
+            printLabel.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLabel.text = "\(enemy.hp) HP"
+        } else {
+            printLabel.text = "Attack Unsuccessful"
+        }
+        
+        //We're using "if let" here because the enemy might not drop any loot (nil!).
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLabel.text = ""
+            printLabel.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
     }
 }
 
