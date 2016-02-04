ee5964c878ec54fed78cee4c4ee136c92ec7ea29
jennojenno <jennolim@gmail.com>
2016-02-04 15:08:54 -0500

    Set up final UI, created vars for necc objects and chest click function

--- rpg-oop/ViewController.swift
+++ rpg-oop/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var printLabel: UILabel!
+    @IBOutlet weak var playerHpLabel: UILabel!
+    @IBOutlet weak var enemyHpLabel: UILabel!
+    @IBOutlet weak var enemyImg: UIImageView!
+    @IBOutlet weak var chestBtn: UIButton!
+    
+    @IBAction func onChestClick(sender: AnyObject) {
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
